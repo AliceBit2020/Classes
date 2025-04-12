aab0206e84ced6e5fea7999cbdbb0fef2f2d12b1
Oleksandra <syatle13@gmail.com>
2025-04-12 13:48:06 +0300

    Add project files.

---
+++ Classes/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharp.Classes1
+{
+    class Car
+    {
+        private string driverName;		// Имя водителя
+        private int currSpeed;			// Текущая скорость
+
+        public Car() : this("Нет водителя", 0)
+        {
+
+        }
+        public Car(Car obj)/////  нет по умолчанию
+        {
+            driverName = obj.driverName;
+            currSpeed = obj.currSpeed;
+        }
+        public Car(string driverName) : this(driverName, 0)
+        {
+        }
+        public Car(string driverName, int speed) // Главный конструктор
+        {
+            this.driverName = driverName;
+            currSpeed = speed;
+        }
+        public void SetDriver(string driverName) =>
+            this.driverName = driverName;
+
+        public void PrintState()		// Распечатка текущих данных
+        {
+            Console.WriteLine("{0} едет со скоростью {1} км/ч.",
+                driverName, currSpeed);
+        }   
+        public void SpeedUp(int delta)	// Увеличение скорости
+        {
+            currSpeed += delta;
+        }
+    }
+
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using  CSharp.Classes1;
+namespace CSharp.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+            st1.studentID = 1;  ////  екземплярне поле
+            //st1.ShowAcademy();
+            //st1academyName;
+
+
+
+
+            Student.ShowAcademy(); // статический метод
+            //Console.WriteLine("Оценка: {0}", st1.GetMark());
+
+            //Console.WriteLine("Конструктор по умолчанию");
+            Car myCar = new Car();
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    myCar.SpeedUp(5);
+            //    myCar.PrintState();
+            //}
+
+            //Console.WriteLine("\n\nКонструктор с параметрами");
+            //myCar = new Car("Рубенс Барикелло");
+
+            //Car obj = new Car(myCar);
+            //obj.PrintState();
+            //myCar.PrintState();
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    myCar.SpeedUp(5);
+            //    myCar.PrintState();
+            //}
+
+            //string s1 = "ddd";
+            //string s2 = s1;
+
+            //s1= "kkk";
+
+            //Console.WriteLine("{0}  {1} ",s1, s2);
+
+
+        }
+    }
+}
